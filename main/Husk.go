48e42a88bf5d45aa7917816965f444d2631f47bf
Júlia Ting Castro Sevilla <juliating03@hotmail.com>
2025-01-09 18:32:52 +0100

    test (sprite not working)

---
+++ main/Husk.go
@@ -0,0 +1,15 @@
+components {
+  id: "test"
+  component: "/scripts/test.script"
+}
+embedded_components {
+  id: "Husk"
+  type: "sprite"
+  data: "default_animation: \"WallpaperDog-20652183\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/main/husk.atlas\"\n"
+  "}\n"
+  ""
+}
